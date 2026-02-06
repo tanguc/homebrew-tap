aed57e5c11cca100fb241e48c8054802ade6340b
stanguc <sergen.tanguc@gmail.com>
2026-02-06 14:52:06 +0100

    monphare 0.1.1: add real SHA256 checksums

--- Formula/monphare.rb
+++ Formula/monphare.rb
@@ -10,20 +10,20 @@ class Monphare < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/tanguc/monphare/releases/download/v#{version}/monphare-darwin-aarch64.tar.gz"
-      sha256 "PLACEHOLDER_DARWIN_ARM64"
+      sha256 "e31117906f2a3cd6075d674faaf7216d6ef183145941a9219121dad41542bdf3"
     else
       url "https://github.com/tanguc/monphare/releases/download/v#{version}/monphare-darwin-x86_64.tar.gz"
-      sha256 "PLACEHOLDER_DARWIN_X86_64"
+      sha256 "2941d84d12c4d5fffe004d76b511ab98e5020cb1fe612add292d52e494a08184"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/tanguc/monphare/releases/download/v#{version}/monphare-linux-aarch64.tar.gz"
-      sha256 "PLACEHOLDER_LINUX_ARM64"
+      sha256 "152ef5cef57dd055d60a3e96ecfa66d0f08c410c92a56e1d9e4266a0b46c4dd6"
     else
       url "https://github.com/tanguc/monphare/releases/download/v#{version}/monphare-linux-x86_64.tar.gz"
-      sha256 "PLACEHOLDER_LINUX_X86_64"
+      sha256 "cd115546ac9e2a2965001c7d4f0f1e2c51d00796abb189dabd00389b01ec2009"
     end
   end
 
